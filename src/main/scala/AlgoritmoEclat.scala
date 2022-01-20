cb047a986d05c616430fb1b444d13a87f4bd1658
LucaAs98 <lucaasunis@libero.it>
2022-01-21 00:32:27 +0100

    Prima prova di algoritmo Eclat

---
+++ src/main/scala/AlgoritmoEclat.scala
@@ -0,0 +1,67 @@
+import java.io.File
+import scala.io.Source
+
+object AlgoritmoEclat extends App {
+  val filePath = "src/main/resources/dataset/datasetKaggleAlimenti.txt"
+  val file = new File(filePath)
+  val scalaFileContentsList = Source.fromFile(file).getLines().toList //Contenuto di tutto il file come lista
+
+  //Lista degli alimenti presenti nel dataset (colonne) senza ripetizioni
+  val listaAlimentiDS = scalaFileContentsList.mkString("").replaceAll("\n", ",").split(",").toList.distinct
+
+  //Creo la lista di transazioni come matrice di zeri e uni
+  val listaTransazioniIntermediaDS = for {
+    list <- scalaFileContentsList
+    alimento <- listaAlimentiDS
+    presente = if (list.contains(alimento)) 1 else 0
+  } yield presente
+
+  //Formatto bene la lista a seconda di quanti alimenti univoci abbiamo
+  val listaTransazioniDS = listaTransazioniIntermediaDS.grouped(listaAlimentiDS.size)
+
+  //Parametro di Eclat
+  val minSupport = 3
+
+  /* Vettore con tutte le combinazioni di alimenti Es: Vector(List(Pane), List(Burro), List(Latte), List(CocaCola), List(Prosciutto), List(Pane, Burro), List(Pane, Latte), ...., List(Pane, Burro, Latte, CocaCola, Prosciutto))
+     Senza ripetizioni */
+  val combinazioniAlimenti = {
+    for {
+      iterazione <- 1 to (listaAlimentiDS.size)
+      tupla <- listaAlimentiDS.combinations(iterazione)
+    } yield tupla
+  }.toList
+
+  //println(combinazioniAlimenti)
+
+  //Restituisce l'indice delle transazioni nella quale è presente tale alimento o tupla di alimenti
+  def getTransationIDs(tupleAlimenti: List[String], transazione: List[Int]): Boolean = {
+    val transitionIDs = for {
+      a <- tupleAlimenti
+    } yield transazione(listaAlimentiDS.indexOf(a))
+
+    val result = transitionIDs forall (x => {
+      x == 1
+    })
+    result
+  }
+
+
+  //Lista degli alimenti (o tuple di alimenti) assieme alle transazioni dove essi appaiono senza che siano raggruppati
+  val listaAlimentiDSTransazioniNonRagg = for {
+    listaAlim <- combinazioniAlimenti
+    transazione <- listaTransazioniDS
+    if (getTransationIDs(listaAlim, transazione))
+  } yield (listaAlim, listaTransazioniDS.indexOf(transazione))
+
+  //Raggruppamento della listaAlimentiDSTransazioniNonRagg per ottenere tutte le transazioni riguardo una certa combinazione
+  val listaAlimTransaz = listaAlimentiDSTransazioniNonRagg.groupBy(_._1).filter(_._2.size >= minSupport)
+
+  //println(listaAlimTransaz.mkString("\n"))
+
+  //Creiamo la lista delle tuple con il relativo numero di transazioni annesso
+  val listaAlimTransazSupport = listaAlimTransaz.map({
+    case (k, v) => k -> v.size
+  })
+
+  //println(listaAlimTransazSupport.mkString("\n"))
+}
